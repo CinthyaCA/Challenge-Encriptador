c33eb4cff7f64af5a1bcbf537a2c40ba2f3d0e52
Cinthya María Cascante Anchía <tinta18.02@gmail.com>
2023-05-22 11:52:33 -0600

    Cambios para colocar texto en cuadro resultado

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ texto.focus()
 
 var mensaje=document.getElementById("mensaje")
 
+var mensajeNoEncontrado=document.getElementById("mensajeNoEncontrado")
+
+var ingreseTexto=document.getElementById("ingreseTexto")
+
 function encriptarTexto (texto){
     texto=texto.replaceAll("e","enter");
     texto=texto.replaceAll ("i","imes");
@@ -17,7 +21,12 @@ function accionEncriptar(){
     mensaje.value = textoEncriptado;
     texto.value="";
     mensaje.style.backgroundImage="none";
+    mensajeNoEncontrado.style.display="none";
+    ingreseTexto.style.display="none";
 }
+
+var botonEncriptar=document.getElementById("encriptar");
+botonEncriptar.onclick=accionEncriptar;
 
 function desencriptarTexto (texto){
     texto=texto.replaceAll("enter","e");
@@ -33,6 +42,8 @@ function accionDesencriptar(){
     mensaje.value = textoDesencriptado;
     texto.value="";
     mensaje.style.backgroundImage="none";
+    mensajeNoEncontrado.style.display="none";
+    ingreseTexto.style.display="none";
 }
 
 function copiarTexto(){
@@ -41,6 +52,9 @@ function copiarTexto(){
     .then(function() {
         mensaje.value=""
         mensaje.style.backgroundImage = "url(Sherlock.png)";
+        mensajeNoEncontrado.style.display="block";
+        ingreseTexto.style.display="block";
+        texto.focus()
     })
 
     .catch(function() {
@@ -49,8 +63,7 @@ function copiarTexto(){
 }
 
 // Botones
-var botonEncriptar=document.getElementById("encriptar");
-botonEncriptar.onclick=accionEncriptar;
+
 
 var botonDesencriptar=document.getElementById("desencriptar");
 botonDesencriptar.onclick=accionDesencriptar;
